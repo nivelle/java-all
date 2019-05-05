50f21df45091b6f2dbe4cd0ae0da099030f9878c
nivellefu <fuxinzhong@zhangyue.com>
2019-05-05 23:01:15 +0800

    【定时任务】

--- src/main/java/com/nivelle/guide/SpringBootAllApplication.java
+++ src/main/java/com/nivelle/guide/SpringBootAllApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@ServletComponentScan
+@ServletComponentScan//扫描servlet组件注解
 @EnableCaching//开启缓存注解,mybatis使用redis作为缓存
+@EnableScheduling//开启定时任务注解
 public class SpringBootAllApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/nivelle/guide/schedule/SchedulerTask.java
@@ -0,0 +1,21 @@
+package com.nivelle.guide.schedule;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SchedulerTask {
+
+
+    private int count = 0;
+
+    @Scheduled(cron = "*/6 * * * * ?")
+    private void process() {
+        System.out.println("this is scheduler task runing  " + (count++));
+    }
+
+    @Scheduled(initialDelay=1000, fixedRate=6000)
+    private void process2() {
+        System.out.println("this is scheduler task runing  " + (count++));
+    }
+}
