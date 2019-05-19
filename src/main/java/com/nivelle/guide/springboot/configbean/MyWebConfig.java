aeaad785af0d9bf3fb419047baed53ec09727d68
nivellefu <fuxinzhong@zhangyue.com>
2019-05-19 10:35:07 +0800

    【内容协调管理器】

--- src/main/java/com/nivelle/guide/springboot/configbean/MyWebConfig.java
+++ src/main/java/com/nivelle/guide/springboot/configbean/MyWebConfig.java
@@ -3,19 +3,60 @@ package com.nivelle.guide.springboot.configbean;
 import com.nivelle.guide.springboot.filter.MyFilter1;
 import com.nivelle.guide.springboot.filter.MyFilter2;
 import com.nivelle.guide.springboot.interceptor.MyInterceptor;
+import com.nivelle.guide.springmvc.PropertiesHandlerMethodArgumentResolver;
+import com.nivelle.guide.springmvc.PropertiesHandlerMethodReturnValueHandler;
+import com.nivelle.guide.springmvc.PropertiesHttpMessageConverter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Configuration
 public class MyWebConfig implements WebMvcConfigurer {
 
+
+    @Autowired
+    private RequestMappingHandlerAdapter requestMappingHandlerAdapter;
+
+
+    @PostConstruct
+    public void init() {
+        // 获取当前 RequestMappingHandlerAdapter 所有的 ArgumentResolver对象
+        List<HandlerMethodArgumentResolver> argumentResolvers = requestMappingHandlerAdapter.getArgumentResolvers();
+        List<HandlerMethodArgumentResolver> newArgumentResolvers = new ArrayList<>(argumentResolvers.size() + 1);
+        // 添加 PropertiesHandlerMethodArgumentResolver 到集合第一个位置
+        newArgumentResolvers.add(0, new PropertiesHandlerMethodArgumentResolver());
+        // 将原 ArgumentResolver 添加到集合中
+        newArgumentResolvers.addAll(argumentResolvers);
+        // 重新设置 ArgumentResolver对象集合
+        requestMappingHandlerAdapter.setArgumentResolvers(newArgumentResolvers);
+
+        // 获取当前 RequestMappingHandlerAdapter 所有的 returnValueHandlers对象
+        List<HandlerMethodReturnValueHandler> returnValueHandlers = requestMappingHandlerAdapter.getReturnValueHandlers();
+        List<HandlerMethodReturnValueHandler> newReturnValueHandlers = new ArrayList<>(returnValueHandlers.size() + 1);
+        // 添加 PropertiesHandlerMethodReturnValueHandler 到集合第一个位置
+        newReturnValueHandlers.add(0, new PropertiesHandlerMethodReturnValueHandler());
+        // 将原 returnValueHandlers 添加到集合中
+        newReturnValueHandlers.addAll(returnValueHandlers);
+        // 重新设置 ReturnValueHandlers对象集合
+        requestMappingHandlerAdapter.setReturnValueHandlers(newReturnValueHandlers);
+    }
+
     /**
      * 请求过滤器
+     *
      * @return
      */
     @SuppressWarnings({"rawtypes", "unchecked"})
@@ -29,8 +70,10 @@ public class MyWebConfig implements WebMvcConfigurer {
         System.out.println("过滤器1注册完成");
         return frBean;
     }
+
     /**
      * 请求过滤器2先与过滤器1执行
+     *
      * @return
      */
     @SuppressWarnings({"rawtypes", "unchecked"})
@@ -46,12 +89,23 @@ public class MyWebConfig implements WebMvcConfigurer {
 
     /**
      * 方法拦截器
+     *
      * @param registry
      */
     @Override
-    public void addInterceptors(InterceptorRegistry registry){
+    public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new MyInterceptor()).addPathPatterns("/config/**");
     }
 
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        // converters.add(new PropertiesHttpMessageConverter());
+        // 指定顺序，这里为第一个
+        converters.add(0, new PropertiesHttpMessageConverter());
+
+    }
 
 }
+
+
+
--- src/main/java/com/nivelle/guide/springmvc/ParameterBindController.java
+++ src/main/java/com/nivelle/guide/springmvc/ParameterBindController.java
@@ -3,14 +3,15 @@ package com.nivelle.guide.springmvc;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.ServletOutputStream;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.util.Properties;
 
 /**
  * springMNC参数绑定学习
@@ -42,19 +43,49 @@ public class ParameterBindController {
 
     /**
      * session/cookie
+     *
      * @param session
      * @return
      */
-    @RequestMapping("/session")
-    public ModelAndView getSession(HttpSession session, Cookie cookie) {
+    @RequestMapping("/session/cookie")
+    public ModelAndView getSession(HttpSession session, HttpServletRequest request) {
 
         String sessionContent = session.getAttribute("sessionParameter").toString();
-        System.out.println("====="+sessionContent);
+        System.out.println("=====" + sessionContent);
 
-        System.out.println("-----"+cookie.getValue());
+        System.out.println("-----" + request.getCookies()[0].getName() + request.getCookies()[0].getValue());
 
         ModelAndView mv = new ModelAndView();
         mv.setViewName("/success");
         return mv;
+    }
+
+    /**
+     * 自定义参数转换器测试,不依赖@RequestBody和 @ResponseBody
+     *
+     * {@link PropertiesHandlerMethodArgumentResolver}
+     * {@link PropertiesHandlerMethodReturnValueHandler}
+     *
+     * @param properties
+     * @return
+     */
+    @RequestMapping(value = "test1", consumes = "text/properties")
+    public Properties getUser1(Properties properties) {
+        System.out.println("入参被解析:properties={}" + properties);
+        return properties;
+    }
+    /**
+     * 自定义参数转换器测试,依赖@RequestBody和 @ResponseBody
+     *
+     * {@link PropertiesHttpMessageConverter}
+     *
+     * @param properties
+     * @return
+     */
+    @RequestMapping(value = "test2", consumes = "text/properties")
+    @ResponseBody
+    public Properties getUser2(@RequestBody Properties properties) {
+        System.out.println("入参被解析:properties={}" + properties);
+        return properties;
     }
 }
