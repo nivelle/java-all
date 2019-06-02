7dd2cf5259663ba6eb18d1e9a8c38d1c385f61de
nivellefu <fuxinzhong@zhangyue.com>
2019-06-02 15:52:41 +0800

    【operation】

---
+++ src/main/java/com/nivelle/guide/java2e/jdk/OperationTest.java
@@ -0,0 +1,15 @@
+package com.nivelle.guide.java2e.jdk;
+
+/**
+ * java运算
+ */
+public class OperationTest {
+
+
+    public static void main(String[] args) {
+        int a = 10;
+        long b = 4;
+        long c = a - b;
+        System.out.println(a - b);
+    }
+}
