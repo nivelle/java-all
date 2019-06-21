908a236fdd45ec010c039d23377938cc6f15b3a7
nivellefu <fuxinzhong@zhangyue.com>
2019-06-21 16:57:13 +0800

    arrayList

--- src/main/java/com/nivelle/guide/java2e/core/ArrayListData.java
+++ src/main/java/com/nivelle/guide/java2e/core/ArrayListData.java
@@ -1,7 +1,7 @@
 package com.nivelle.guide.java2e.core;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  * ArrayList
@@ -17,7 +17,7 @@ public class ArrayListData {
      *
      * @param args
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         /**
          *  Resizable【可变】-array implementation of the <tt>List</tt> interface.  Implements all optional list operations,
@@ -29,13 +29,13 @@ public class ArrayListData {
         /**
          * 可以存放任意类型的元素
          */
-        List list = new ArrayList();
-        list.add(7);
-        list.add(MyEnum.ONE);
-        list.add("nivelle");
-        list.add(null);
-        list.add(null);
-        System.out.println(list);
+        ArrayList arrayList = new ArrayList();
+        arrayList.add(7);
+        arrayList.add(MyEnum.ONE);
+        arrayList.add("nivelle");
+        arrayList.add(null);
+        arrayList.add(null);
+        System.out.println(arrayList);
 
         /**
          * ArrayList数据底层都保存在Object[]数组中,而且空集合会在第一次添加元素的时候进行底层扩展
@@ -46,7 +46,44 @@ public class ArrayListData {
          * will be expanded to DEFAULT_CAPACITY when the first element is added.
          **/
         //transient Object[] elementData; // non-private to simplify nested class access
-        List list1 = new ArrayList(18);
+        ArrayList arrayList2 = new ArrayList(18);
+        arrayList2.add(null);
+
+        /**
+         * The size of the ArrayList (the number of elements it contains).
+         * size返回的是数组中元素的数量
+         */
+        System.out.println("当前list集合的大小size():" + arrayList.size());
+
+
+        Field field = arrayList2.getClass().getDeclaredField("elementData");
+        field.setAccessible(true);
+        Object[] result = (Object[]) field.get(arrayList2);
+        System.out.println("通过反射获取当前 arrayList2 的容量大小" + result.length);
+        System.out.println("当前 arrayList2 集合的大小size():" + arrayList2.size());
+        /**
+         * 释放没有使用的空间容量,会导致modCount +1
+         */
+        arrayList2.trimToSize();
+        Object[] trimResult = (Object[]) field.get(arrayList2);
+        System.out.println("trimToSize:通过反射获取当前arrayList的容量大小" + trimResult.length);
+        System.out.println("当前 arrayList2 集合的大小size():" + arrayList2.size());
+
+        /**
+         * 如果不是初始空集合,则按照指定的minCapacity进行扩容;如果是初始空集合,则只有大于默认的容量
+         *
+         * DEFAULT_CAPACITY = 10 才进行扩容,否则不扩容。
+         */
+        ArrayList arrayList3 = new ArrayList();
+        //必须进行一次添加操纵才有默认的容量
+        arrayList3.add(null);
+        arrayList3.ensureCapacity(9);
+        Field field3 = arrayList3.getClass().getDeclaredField("elementData");
+        field3.setAccessible(true);
+        Object[] ensureResult = (Object[]) field3.get(arrayList3);
+        System.out.println("ensureCapacity:通过反射获取当前 arrayList3 的容量大小" + ensureResult.length);
+        System.out.println("当前 arrayList3 集合的大小size():" + arrayList3.size());
+
 
     }
 }
