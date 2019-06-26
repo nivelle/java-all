541fd862fcd9fc91c56f382011676106aa772337
nivellefu <fuxinzhong@zhangyue.com>
2019-06-26 19:50:58 +0800

    【HashMap】

--- src/main/java/com/nivelle/guide/java2e/jdk/HashMapData.java
+++ src/main/java/com/nivelle/guide/java2e/jdk/HashMapData.java
@@ -138,6 +138,131 @@ public class HashMapData {
 
         System.out.println(hashMap1);
 
+
+        /**
+         * final V putVal(int hash, K key, V value, boolean onlyIfAbsent,
+         *                    boolean evict) {
+         *         Node<K,V>[] tab; Node<K,V> p; int n, i;
+         *         if ((tab = table) == null || (n = tab.length) == 0)
+         *             n = (tab = resize()).length;
+         *         if ((p = tab[i = (n - 1) & hash]) == null)
+         *             tab[i] = newNode(hash, key, value, null);
+         *         else {
+         *             Node<K,V> e; K k;
+         *             if (p.hash == hash &&
+         *                 ((k = p.key) == key || (key != null && key.equals(k))))
+         *                 e = p;
+         *             else if (p instanceof TreeNode)
+         *                 e = ((TreeNode<K,V>)p).putTreeVal(this, tab, hash, key, value);
+         *             else {
+         *                 for (int binCount = 0; ; ++binCount) {
+         *                     if ((e = p.next) == null) {
+         *                         p.next = newNode(hash, key, value, null);
+         *                         if (binCount >= TREEIFY_THRESHOLD - 1) // -1 for 1st
+         *                             treeifyBin(tab, hash);
+         *                         break;
+         *                     }
+         *                     if (e.hash == hash &&
+         *                         ((k = e.key) == key || (key != null && key.equals(k))))
+         *                         break;
+         *                     p = e;
+         *                 }
+         *             }
+         *             if (e != null) { // existing mapping for key
+         *                 V oldValue = e.value;
+         *                 if (!onlyIfAbsent || oldValue == null)
+         *                     e.value = value;
+         *                 afterNodeAccess(e);
+         *                 return oldValue;
+         *             }
+         *         }
+         *         ++modCount;
+         *         if (++size > threshold)
+         *             resize();
+         *         afterNodeInsertion(evict);
+         *         return null;
+         *     }
+         */
+
+
+        /**
+         *   final Node<K,V>[] resize() {
+         *         Node<K,V>[] oldTab = table;
+         *         int oldCap = (oldTab == null) ? 0 : oldTab.length;
+         *         int oldThr = threshold;
+         *         int newCap, newThr = 0;
+         *         if (oldCap > 0) {
+         *             if (oldCap >= MAXIMUM_CAPACITY) {
+         *                 threshold = Integer.MAX_VALUE;
+         *                 return oldTab;
+         *             }
+         *             else if ((newCap = oldCap << 1) < MAXIMUM_CAPACITY &&
+         *                      oldCap >= DEFAULT_INITIAL_CAPACITY)
+         *                 newThr = oldThr << 1; // double threshold
+         *         }
+         *         else if (oldThr > 0) // initial capacity was placed in threshold
+         *             newCap = oldThr;
+         *         else {               // zero initial threshold signifies using defaults
+         *             newCap = DEFAULT_INITIAL_CAPACITY;
+         *             newThr = (int)(DEFAULT_LOAD_FACTOR * DEFAULT_INITIAL_CAPACITY);
+         *         }
+         *         if (newThr == 0) {
+         *             float ft = (float)newCap * loadFactor;
+         *             newThr = (newCap < MAXIMUM_CAPACITY && ft < (float)MAXIMUM_CAPACITY ?
+         *                       (int)ft : Integer.MAX_VALUE);
+         *         }
+         *         threshold = newThr;
+         *         @SuppressWarnings({"rawtypes","unchecked"})
+         *             Node<K , V>[] newTab = (Node<K,V>[])new Node[newCap];
+         *         table = newTab;
+         *         if (oldTab != null) {
+         *             for (int j = 0; j < oldCap; ++j) {
+         *                 Node<K,V> e;
+         *                 if ((e = oldTab[j]) != null) {
+         *                     oldTab[j] = null;
+         *                     if (e.next == null)
+         *                         newTab[e.hash & (newCap - 1)] = e;
+         *                     else if (e instanceof TreeNode)
+         *                         ((TreeNode<K,V>)e).split(this, newTab, j, oldCap);
+         *                     else { // preserve order
+         *                         Node<K,V> loHead = null, loTail = null;
+         *                         Node<K,V> hiHead = null, hiTail = null;
+         *                         Node<K,V> next;
+         *                         do {
+         *                             next = e.next;
+         *                             if ((e.hash & oldCap) == 0) {
+         *                                 if (loTail == null)
+         *                                     loHead = e;
+         *                                 else
+         *                                     loTail.next = e;
+         *                                 loTail = e;
+         *                             }
+         *                             else {
+         *                                 if (hiTail == null)
+         *                                     hiHead = e;
+         *                                 else
+         *                                     hiTail.next = e;
+         *                                 hiTail = e;
+         *                             }
+         *                         } while ((e = next) != null);
+         *                         if (loTail != null) {
+         *                             loTail.next = null;
+         *                             newTab[j] = loHead;
+         *                         }
+         *                         if (hiTail != null) {
+         *                             hiTail.next = null;
+         *                             newTab[j + oldCap] = hiHead;
+         *                         }
+         *                     }
+         *                 }
+         *             }
+         *         }
+         *         return newTab;
+         *     }
+         */
+
+
+
     }
 
 
