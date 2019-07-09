b47151921d5bac6e2850d3b5330021cee1b0695d
nivellefu <fuxinzhong@zhangyue.com>
2019-07-09 15:43:37 +0800

    【fix】

--- src/main/java/com/nivelle/guide/distributed/lock/CuratorController.java
+++ src/main/java/com/nivelle/guide/distributed/lock/CuratorController.java
@@ -7,7 +7,6 @@ package com.nivelle.guide.distributed.lock;
  * @date 2019/07/06
  */
 
-import com.nivelle.guide.distributed.lock.DistributedLockByCurator;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
