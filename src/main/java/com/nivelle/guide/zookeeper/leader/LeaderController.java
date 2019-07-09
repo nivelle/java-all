99514af1e850f45c11d35c25f9688b1537204b58
nivellefu <fuxinzhong@zhangyue.com>
2019-07-09 22:24:26 +0800

    【leader】

--- src/main/java/com/nivelle/guide/zookeeper/leader/LeaderController.java
+++ src/main/java/com/nivelle/guide/zookeeper/leader/LeaderController.java
@@ -2,6 +2,7 @@ package com.nivelle.guide.zookeeper.leader;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.recipes.leader.LeaderLatch;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +21,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class LeaderController {
 
     private static final String PATH = "/zk/leader";
+    private static final String PATH2 = "/zk/leader2";
+
 
     @Autowired
     @Lazy
@@ -32,6 +35,31 @@ public class LeaderController {
         example.start();
         SelectorClient example2 = new SelectorClient(curatorFramework, PATH, "fuck");
         example2.start();
+        return "success";
+    }
+
+    @RequestMapping("leaderLatch")
+    public String leaderLatch() {
+        try {
+            LeaderLatch leaderLatch1 = new LeaderLatch(curatorFramework, PATH, "jessy");
+            leaderLatch1.start();
+            LeaderLatch leaderLatch2 = new LeaderLatch(curatorFramework, PATH, "fuck");
+            leaderLatch2.start();
+            Boolean isLeader = leaderLatch1.hasLeadership();
+            if (isLeader) {
+                log.info(leaderLatch1.getId() + "is leader");
+                leaderLatch1.close();
+            }else {
+                leaderLatch1.await();
+            }
+            Boolean isLeader2 = leaderLatch2.hasLeadership();
+            if (isLeader2) {
+                log.info(leaderLatch2.getId() + "is leader");
+                leaderLatch2.close();
+            }
+        } catch (Exception e) {
+            log.error("leaderLatch is error ", e);
+        }
         return "success";
     }
 }
