470ca92a1020e813fcc8c29ff17fe9f705fce640
nivellefu <fuxinzhong@zhangyue.com>
2019-07-11 19:02:30 +0800

    【ThreadPool】

--- src/main/java/com/nivelle/guide/datastructures/ThreadPoolData.java
+++ src/main/java/com/nivelle/guide/datastructures/ThreadPoolData.java
@@ -1,8 +1,6 @@
 package com.nivelle.guide.datastructures;
 
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
@@ -27,24 +25,63 @@ public class ThreadPoolData {
          */
 
         /**
-         * corePoolSize:默认等待有任务到来才创建线程去执行任务，调用preStartAllCoreThreads()或preStartCoreThread()在没有任务到来之前就创建corePoolSize个线程或者一个线程。
+         * 1. corePoolSize:默认等待有任务到来才创建线程去执行任务，调用preStartAllCoreThreads()或p
+         * reStartCoreThread()在没有任务到来之前就创建corePoolSize个线程或者一个线程。//even if other worker threads are idle
          *
-         * keepAliveTime:如果线程池中的线程数量大于 corePoolSize时，如果某线程空闲时间超过keepAliveTime，线程将被终止，直至线程池中的线程数目不大于corePoolSize；
-         * 如果设置allowCoreThreadTimeOut，那么核心池中的线程空闲时间超过 keepAliveTime，线程也会被终止。
-         */
-
+         * 2. keepAliveTime:如果线程池中的线程数量大于 corePoolSize时，如果某线程空闲时间超过keepAliveTime，线程将被终止，直至线程池中的线程数目不大于corePoolSize；
+         * 如果设置 allowCoreThreadTimeOut，那么核心池中的线程空闲时间超过 keepAliveTime，线程也会被终止。
+         *
+         * 3. 默认的RejectedExecutionHandler 是AbortPolicy,拒绝任务同时抛出 RejectedExecutionException
+         *
+         * 总共有四种拒绝策略:
+         * ThreadPoolExecutor.AbortPolicy:丢弃任务并抛出RejectedExecutionException异常。
+         * ThreadPoolExecutor.DiscardPolicy：也是丢弃任务，但是不抛出异常。
+         * ThreadPoolExecutor.DiscardOldestPolicy：丢弃队列最前面的任务，然后重新尝试执行任务（重复此过程）
+         * ThreadPoolExecutor.CallerRunsPolicy：由调用线程处理该任务
+         *
+         * 4. 队列
+         * - SynchronousQueue
+         * - LinkedBlockingQueue
+         * - ArrayBlockingQueue
+         *
+         * */
+        Thread.currentThread().setPriority(Thread.MIN_PRIORITY);
         ThreadPoolExecutor executor =
                 new ThreadPoolExecutor(5, 10, 200, TimeUnit.MILLISECONDS,
                         new ArrayBlockingQueue(5));
-
+        // 提前启动所有的核心线程数目线程
+        executor.prestartAllCoreThreads();
+        //核心线程数目也可以设置过期时间
+        executor.allowCoreThreadTimeOut(true);
         for (int i = 0; i < 15; i++) {
             MyTask myTask = new MyTask(i, threadLocal);
             executor.execute(myTask);
+            if (i == 3) {
+                //corePoolSize 和 maxMumPoolSize是可以调节的
+                executor.setCorePoolSize(5);
+                executor.setMaximumPoolSize(15);
+                // keepAliveTime 可以动态设置
+                executor.setKeepAliveTime(10, TimeUnit.MILLISECONDS);
+            }
             System.out.println("线程池中线程数目：" + executor.getPoolSize() + "，队列中等待执行的任务数目：" +
                     executor.getQueue().size() + "，已执行完成的任务数目：" + executor.getCompletedTaskCount());
         }
-        System.out.println("main 线程中的值:"+threadLocal.get());
+        System.out.println("main 线程中的值:" + threadLocal.get());
         executor.shutdown();
+        int priority = Thread.currentThread().getPriority();
+        System.err.println("当前线程的优先级,priority=" + priority);
+    }
+
+
+    public static void ExecutorsThreadFactory() {
+
+        /**
+         * 线程池默认提供的线程工程实现,创建的线程优先级为Thread.NORMAL = 5;非守护线程；
+         *
+         * if a ThreadFactory ails to create a thread when asked by returning null from {@code newThread}, the executor will continue, but might not be able to execute any tasks.
+         */
+        ThreadFactory threadFactory = Executors.defaultThreadFactory();
+
     }
 }
 
@@ -60,15 +97,24 @@ class MyTask implements Runnable {
 
     @Override
     public void run() {
-        System.out.println("正在执行task " + taskNum+"修改自己的变量值:");
+        System.out.println("正在执行task " + taskNum + "修改自己的变量值:");
         threadLocal.set(taskNum);
         try {
             Thread.sleep(500);
         } catch (InterruptedException e) {
             System.out.println(e);
         }
-        System.out.println(taskNum+":当前线程中的数据:" + threadLocal.get());
-        System.out.println("task " + taskNum + "执行完毕");
+        System.out.println(taskNum + ":当前线程中的数据:" + threadLocal.get());
+        /**
+         * threadName:
+         * New threads have names accessible via  of pool-N-thread-M</em>, where <em>N</em> is the sequence
+         * number of this factory, and <em>M</em> is the sequence number of the thread created by this factory.
+         */
+        System.out.println("task " + Thread.currentThread().getName() + taskNum + "执行完毕");
+        Thread.currentThread().interrupt();
+
     }
+
+
 }
 
