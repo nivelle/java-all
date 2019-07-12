6a12d41bd754148d05201ae74dc9b7b435d1f84d
nivellefu <fuxinzhong@zhangyue.com>
2019-07-12 11:53:53 +0800

    【Synchronized】

--- src/main/java/com/nivelle/guide/datastructures/SynchronizedData.java
+++ src/main/java/com/nivelle/guide/datastructures/SynchronizedData.java
@@ -1,10 +1,10 @@
 package com.nivelle.guide.datastructures;
 
 /**
- * TODO:DOCUMENT ME!
+ * Synchronized
  *
  * @author fuxinzhong
  * @date 2019/06/16
  */
-public class synchronizedData {
+public class SynchronizedData {
 }
