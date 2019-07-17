8b14633dff55f6a6f4bdaddbaee226e8d23e9272
nivellefu <fuxinzhong@zhangyue.com>
2019-07-17 18:43:32 +0800

    【threadPool&ConcurrentHashMap】

--- src/main/java/com/nivelle/guide/datastructures/ConcurrentHashMapData.java
+++ src/main/java/com/nivelle/guide/datastructures/ConcurrentHashMapData.java
@@ -1,5 +1,13 @@
 package com.nivelle.guide.datastructures;
 
+import java.util.HashMap;
+import java.util.Random;
+import java.util.UUID;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * ConcurrentHashMap
  *
@@ -7,4 +15,68 @@ package com.nivelle.guide.datastructures;
  * @date 2019/06/16
  */
 public class ConcurrentHashMapData {
+
+    public static void main(String[] args) {
+
+        ThreadPoolExecutor executor =
+                new ThreadPoolExecutor(5, 10, 200, TimeUnit.MILLISECONDS,
+                        new ArrayBlockingQueue(5), new ThreadPoolExecutor.CallerRunsPolicy());
+        /**
+         * 同步HashMap
+         */
+        final HashMap<AtomicInteger, String> map = new HashMap(2);
+        for (int i = 0; i < 1000; i++) {
+            executor.execute(new MyTask1(map));
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * 线程安全异常模拟
+ */
+class MyTask1 implements Runnable {
+    HashMap<AtomicInteger, String> map;
+
+    AtomicInteger atomicInteger = new AtomicInteger(1);
+
+    public MyTask1(HashMap<AtomicInteger, String> map) {
+        this.map = map;
+    }
+    @Override
+    public void run() {
+        try {
+            Random random = new Random();
+            int rand = random.nextInt(100);
+
+            if (rand < 50) {
+                map.put(atomicInteger, UUID.randomUUID().toString());
+                atomicInteger.incrementAndGet();
+            } else {
+                map.get(UUID.randomUUID().toString());
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        System.out.println(map);
+    }
 }
