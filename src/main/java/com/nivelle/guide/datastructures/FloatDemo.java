0a71a85f38d922c82f2a5b53f0719c314bd50210
nivellefu <fuxinzhong@zhangyue.com>
2019-07-22 09:47:33 +0800

    【Float】

--- src/main/java/com/nivelle/guide/datastructures/FloatDemo.java
+++ src/main/java/com/nivelle/guide/datastructures/FloatDemo.java
@@ -9,9 +9,9 @@ package com.nivelle.guide.datastructures;
 public class FloatDemo {
 
     public static void main(String[] args) {
-        Float myFloat = new Float(0.2F);
-        Float myFloatDouble = new Float(0.25);
-        Float myFloatString = new Float("0.255");
+        Float myFloat = new Float(0.6F);
+        Float myFloatDouble = new Float(0.5);
+        Float myFloatString = new Float("0.655");
 
         System.out.println("myFloat = " + myFloat);
         System.out.println("myFloatDouble = " + myFloatDouble);
@@ -20,6 +20,10 @@ public class FloatDemo {
         System.out.println("int value = " + myFloat.intValue());
         System.out.println("double value = " + myFloat.doubleValue());
         System.out.println("float value = " + myFloat.floatValue());
+        System.out.println("myFloatDouble short value = " + myFloatDouble.shortValue());
+        System.out.println("myFloat short value = " + myFloat.shortValue());
+        System.out.println("myFloatString short value = " + myFloatString.shortValue());
+
 
         System.out.println("max value:" + Float.MAX_VALUE);
         System.out.println("max value normal:" + Float.MAX_VALUE);
@@ -34,6 +38,7 @@ public class FloatDemo {
         System.out.println("是否是有限:" + Float.isFinite(myFloat));
         System.out.println("是否是有限:" + Float.isFinite(Float.NEGATIVE_INFINITY));
 
+        System.out.println(Float.toHexString(myFloat));
 
 
     }
