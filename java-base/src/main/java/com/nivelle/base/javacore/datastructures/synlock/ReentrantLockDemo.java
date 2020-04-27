377c536e1186a47e0ac4b98c2ad18d8cd372ecdc
fuxinzhong <fuxinzhong@zhangyue.com>
2020-04-27 21:08:10 +0800

    copyOnWriterArrayList

--- java-base/src/main/java/com/nivelle/base/javacore/datastructures/synlock/ReentrantLockDemo.java
+++ java-base/src/main/java/com/nivelle/base/javacore/datastructures/synlock/ReentrantLockDemo.java
@@ -3,7 +3,7 @@ package com.nivelle.base.javacore.datastructures.synlock;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
- * ReentrantLock
+ * ReentrantLock 源码分析
  *
  * @author nivell
  * @date 2019/06/16
@@ -12,5 +12,9 @@ public class ReentrantLockDemo {
 
     public static void main(String[] args) {
         ReentrantLock reentrantLock = new ReentrantLock();
+
+        reentrantLock.lock();
+        reentrantLock.unlock();
+        reentrantLock.newCondition();
     }
 }
