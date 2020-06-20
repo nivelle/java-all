2155053da32b7e316319c273ba2c11f5f605488b
fuxinzhong <fuxinzhong@zhangyue.com>
2020-06-20 23:13:32 +0800

    netty

--- rpc-dubbo/src/main/java/com/nivelle/rpc/netty/base/client/NettyClient.java
+++ rpc-dubbo/src/main/java/com/nivelle/rpc/netty/base/client/NettyClient.java
@@ -36,7 +36,14 @@ public class NettyClient {
     public static void main(String[] args) throws InterruptedException, IOException {
         System.out.println("客户端成功启动...");
         b.group(groupBoss);
+        /**
+         * “channel()方法指定了Channel的实现类。如果该实现类没提供默认的构造函数，可以通过调用channel- Factory()方法来指定一个工厂类，它将会被bind()方法调用”
+         */
         b.channel(NioSocketChannel.class);
+        /**
+         * “指定Channel应该绑定到的本地地址。如果没有指定，则将由操作系统创建一个随机的地址。或者，也可以通过”“bind()或者connect()方法指定localAddress”
+         */
+        //b.bind(new LocalAddress("127.0.0.1:6789"));
         //channelInitializer 注册到 bootstrap
         b.handler(new NettyClientFilter());
         // 连接服务端,阻塞等待直到连接完成
