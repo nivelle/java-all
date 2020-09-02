ee405e90f4e6dd009749cff5f1c5c2cd28ec99aa
fuxinzhong <fuxinzhong@zhangyue.com>
2020-09-02 23:48:43 +0800

    collections

--- java-base/src/main/java/com/nivelle/base/utils/ArraysTest.java
+++ java-base/src/main/java/com/nivelle/base/utils/ArraysTest.java
@@ -1,6 +1,7 @@
 package com.nivelle.base.utils;
 
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * Arrays
@@ -10,14 +11,33 @@ import java.util.Arrays;
  */
 public class ArraysTest {
     public static void main(String[] args) {
-        int[] test = new int[]{1, 3, 9, 10, 2};
-        Arrays.sort(test);
-        Arrays.stream(test).forEach(System.out::println);
+        Integer[] test1 = new Integer[]{1, 3, 9, 10, 2};
+        /**
+         * 排序
+         */
+        Arrays.sort(test1);
+        /**
+         * 转流
+         */
+        Arrays.stream(test1).forEach(System.out::println);
+        System.out.println("========");
+        /**
+         * 转集合
+         */
+        List<Integer> list = Arrays.asList(test1);
+        System.out.println(list);
 
-        System.out.println(Arrays.asList(test).get(1));
-
-
-
-
+        Integer[] test2 = new Integer[]{1, 3, 9, 10, 2};
+        /**
+         * 比较数组是否相等
+         */
+        boolean equals = Arrays.equals(test1, test2);
+        System.out.println(equals);
+        /**
+         * 数组拷贝
+         */
+        Integer[] copyArray = Arrays.copyOfRange(test1, 1, test1.length);
+        System.out.println(copyArray);
+        System.out.println(Arrays.asList(copyArray));
     }
 }
