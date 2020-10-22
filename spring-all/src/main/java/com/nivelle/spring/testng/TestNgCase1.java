da1e8204c62dc96edbbaf77d6eb9a4cf56096f52
fuxinzhong <fuxinzhong@zhangyue.com>
2020-10-22 08:04:16 +0800

    testNg

---
+++ spring-all/src/main/java/com/nivelle/spring/testng/TestNgCase1.java
@@ -0,0 +1,34 @@
+package com.nivelle.spring.testng;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * TestNg
+ *
+ * @author fuxinzhong
+ * @date 2020/10/21
+ */
+public class TestNgCase1 {
+
+
+    @Test
+    public void f1() {
+        System.out.println("test1 ng");
+        Assert.assertTrue(true);
+    }
+
+    /**
+     * 超时设置
+     *
+     * @throws Exception
+     */
+    @Test(timeOut = 1000)
+    public void f2() throws Exception {
+        System.out.println("test2 ng");
+        Thread.sleep(1500);
+        Assert.assertTrue(true);
+    }
+
+
+}
