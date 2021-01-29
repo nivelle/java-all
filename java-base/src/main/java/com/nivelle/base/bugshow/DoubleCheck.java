5ec0fe679cee632807a5ffe4563a2e69b814839a
fuxinzhong <fuxinzhong@zhangyue.com>
2021-01-29 16:35:51 +0800

    java

---
+++ java-base/src/main/java/com/nivelle/base/bugshow/DoubleCheck.java
@@ -0,0 +1,48 @@
+package com.nivelle.base.bugshow;
+
+/**
+ * 双重检查问题
+ *
+ * @author fuxinzhong
+ * @date 2021/01/29
+ */
+public class DoubleCheck {
+
+    static DoubleCheck doubleCheck;
+
+    static DoubleCheck getInstance() {
+        if (doubleCheck == null) {
+            synchronized (DoubleCheck.class) {
+                if (doubleCheck == null) {
+                    doubleCheck = new DoubleCheck();
+                }
+            }
+        }
+        return doubleCheck;
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 1000; i++) {
+            Thread thread1 = new Thread(() -> {
+                DoubleCheck doubleCheck = DoubleCheck.getInstance();
+                if (doubleCheck == null) {
+                    System.err.println(Thread.currentThread().getName() + "doubleCheck is null");
+                } else {
+                    System.out.println(Thread.currentThread().getName() + "doubleCheck is not null");
+                }
+            });
+            Thread thread2 = new Thread(() -> {
+                DoubleCheck doubleCheck = DoubleCheck.getInstance();
+                if (doubleCheck == null) {
+                    System.err.println(Thread.currentThread().getName() + "doubleCheck is null");
+                } else {
+                    System.out.println(Thread.currentThread().getName() + "doubleCheck is not null");
+                }
+            });
+
+            thread1.start();
+            thread2.start();
+        }
+
+    }
+}
