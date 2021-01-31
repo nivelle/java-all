e27d4bd6fe3281ef09abe6c1b179fb851644fc25
fuxinzhong <fuxinzhong@zhangyue.com>
2021-01-31 22:25:46 +0800

    java

--- java-base/src/main/java/com/nivelle/base/jdk/lang/ThreadMethodMock.java
+++ java-base/src/main/java/com/nivelle/base/jdk/lang/ThreadMethodMock.java
@@ -55,7 +55,8 @@ public class ThreadMethodMock {
         System.out.println("守护线程 demo 结束");
         System.out.println("thread yield demo 方法开始");
         /**
-         * 使当前线程从执行状态（运行状态）变为可执行态（就绪状态）。cpu会从众多的可执行态里选择，也就是说，当前也就是刚刚的那个线程还是有可能会被再次执行到的，并不是说一定会执行其他线程而该线程在下一次中不会执行到了。
+         * 使当前线程从执行状态（运行状态）变为可执行态（就绪状态）。
+         * cpu会从众多的可执行态里选择，也就是说，当前也就是刚刚的那个线程还是有可能会被再次执行到的，并不是说一定会执行其他线程而该线程在下一次中不会执行到了。
          */
         Thread.yield();
         System.out.println(Thread.currentThread().getName() + "让出CPU执行权限");
