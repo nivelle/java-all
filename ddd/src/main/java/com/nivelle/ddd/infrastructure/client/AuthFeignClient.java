48e7d2968f42e0ffa99042bd788617b53175f07d
fuxinzhong <fuxinzhong@zhangyue.com>
2021-02-20 08:46:40 +0800

    ddd

--- ddd/src/main/java/com/nivelle/ddd/infrastructure/client/AuthFeignClient.java
+++ ddd/src/main/java/com/nivelle/ddd/infrastructure/client/AuthFeignClient.java
@@ -1,7 +1,7 @@
 package com.nivelle.ddd.infrastructure.client;
 
+import com.nivelle.ddd.domain.person.entity.Person;
 import com.nivelle.ddd.infrastructure.common.api.Response;
-import ddd.leave.domain.person.entity.Person;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 
