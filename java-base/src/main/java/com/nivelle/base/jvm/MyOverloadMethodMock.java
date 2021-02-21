dcdb4a28b032c4ffa85b6cd0f3c98a327d3a7da9
fuxinzhong <fuxinzhong@zhangyue.com>
2021-02-21 23:49:44 +0800

    jvm

--- java-base/src/main/java/com/nivelle/base/jvm/MyOverloadMethodMock.java
+++ java-base/src/main/java/com/nivelle/base/jvm/MyOverloadMethodMock.java
@@ -10,7 +10,7 @@ public class MyOverloadMethodMock {
 
     public static void main(String[] args) {
         /**
-         * 方法重载支持 包装类
+         * 方法重载支持 包装类：先不考虑包装类进行精确匹配，如果没有完全精确匹配则考虑装箱类
          */
         MyOverloadMethod myOverloadMethod = new MyOverloadMethod();
         long result1 = myOverloadMethod.myMethod("2", new Integer(1));
@@ -21,6 +21,9 @@ public class MyOverloadMethodMock {
         myOverloadMethod.myMethod1(null, 1);
         myOverloadMethod.myMethod1(null, new Object[]{1});
         //myOverloadMethod.myMethod1(null,1,2);
+
+        myOverloadMethod.myMethod3(1L, 2);
+        myOverloadMethod.myMethod3(1L, 2, 1);
 
 
     }
