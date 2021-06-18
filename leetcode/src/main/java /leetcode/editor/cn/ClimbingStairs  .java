179e8fa5304328796d82679883f46bc568f72d79
fuxinzhong <fuxinzhong@zhangyue.com>
2021-06-18 22:23:30 +0800

    爬楼梯

---
+++ leetcode/src/main/java /leetcode/editor/cn/ClimbingStairs  .java
@@ -0,0 +1,46 @@
+//假设你正在爬楼梯。需要 n 阶你才能到达楼顶。 
+//
+// 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？ 
+//
+// 注意：给定 n 是一个正整数。 
+//
+// 示例 1： 
+//
+// 输入： 2
+//输出： 2
+//解释： 有两种方法可以爬到楼顶。
+//1.  1 阶 + 1 阶
+//2.  2 阶 
+//
+// 示例 2： 
+//
+// 输入： 3
+//输出： 3
+//解释： 有三种方法可以爬到楼顶。
+//1.  1 阶 + 1 阶 + 1 阶
+//2.  1 阶 + 2 阶
+//3.  2 阶 + 1 阶
+// 
+// Related Topics 动态规划 
+// 👍 1700 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int climbStairs(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        int[] sum = new int[n];
+        sum[0] = 1;
+        sum[1] = 2;
+        for (int i = 2; i < n; i++) {
+            sum[i] = sum[i - 1] + sum[i - 2];
+        }
+        return sum[n - 1];
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
