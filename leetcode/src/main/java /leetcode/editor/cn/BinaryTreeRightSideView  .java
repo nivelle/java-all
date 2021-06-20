7d4c5db35cfbc553ff364236ef616c8fe7f7e2fa
fuxinzhong <fuxinzhong@zhangyue.com>
2021-06-20 19:48:15 +0800

    二分查找算法

---
+++ leetcode/src/main/java /leetcode/editor/cn/BinaryTreeRightSideView  .java
@@ -0,0 +1,40 @@
+//给定一棵二叉树，想象自己站在它的右侧，按照从顶部到底部的顺序，返回从右侧所能看到的节点值。 
+//
+// 示例: 
+//
+// 输入: [1,2,3,null,5,null,4]
+//输出: [1, 3, 4]
+//解释:
+//
+//   1            <---
+// /   \
+//2     3         <---
+// \     \
+//  5     4       <---
+// 
+// Related Topics 树 深度优先搜索 广度优先搜索 递归 队列 
+// 👍 480 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
