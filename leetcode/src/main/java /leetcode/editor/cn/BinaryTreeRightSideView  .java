545fb985d32d86966c5064915691b285089c17d0
fuxinzhong <fuxinzhong@zhangyue.com>
2021-06-21 17:52:29 +0800

    二叉树右视图

--- leetcode/src/main/java /leetcode/editor/cn/BinaryTreeRightSideView  .java
+++ leetcode/src/main/java /leetcode/editor/cn/BinaryTreeRightSideView  .java
@@ -17,26 +17,86 @@
 
 
 //leetcode submit region begin(Prohibit modification and deletion)
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
-       //DFS（Deep First Search）深度优先搜索。
-        
+        // DFS（Deep First Search）深度优先搜索。
+
         //BFS（Breath First Search）广度优先搜索。
+        // return bfsSearch(root);
+        return dfsSearch(root);
+    }
+
+    //广度优先遍历
+    private List<Integer> bfsSearch(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        if (root == null) {
+            return res;
+        }
+        LinkedList<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int len = queue.size();
+            for (int i = 0; i < len; i++) {
+                TreeNode treeNode = queue.poll();
+                if (treeNode.left != null) {
+                    queue.add(treeNode.left);
+                }
+                if (treeNode.right != null) {
+                    queue.add(treeNode.right);
+                }
+                //最后一个节点正好是右节点
+                if (i == len - 1) {
+                    //将这一层节点从左到右加入队列，将这一层的最后一个节点放入res
+                    res.add(treeNode.val);
+                }
+            }
+        }
+        return res;
+
+    }
+
+    List<Integer> res2 = new ArrayList<Integer>();
+
+    //深度优先遍历
+    private List<Integer> dfsSearch(TreeNode root) {
+        if (root == null) {
+            return res2;
+        }
+        int level = 0;
+        dfs(root, level);
+        return res2;
+    }
+
+    private void dfs(TreeNode root, int level) {
+        if (root == null) return;
+
+        if (level == res2.size()) {
+            res2.add(root.val);
+        }
+        level++;
+        //先遍历右子树
+        dfs(root.right, level);
+        dfs(root.left, level);
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
