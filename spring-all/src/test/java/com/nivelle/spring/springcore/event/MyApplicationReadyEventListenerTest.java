780d5042f55ec7cb45e815d8223f0c67791ce531
fuxinzhong <fuxinzhong@zhangyue.com>
2021-09-06 15:12:37 +0800

    【Spring beanDefinition】

---
+++ spring-all/src/test/java/com/nivelle/spring/springcore/event/MyApplicationReadyEventListenerTest.java
@@ -0,0 +1,10 @@
+package com.nivelle.spring.springcore.event;
+
+import static org.junit.jupiter.api.Assertions.*;
+ /**
+ * TODO:DOCUMENT ME!
+ * @author fuxinzhong
+ * @date 2021/09/06
+ */class MyApplicationReadyEventListenerTest {
+  
+}
