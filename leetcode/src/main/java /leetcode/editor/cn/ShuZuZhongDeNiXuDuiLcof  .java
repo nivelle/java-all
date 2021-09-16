881dc1481efe4e6b18fb8d089739dc3b7bec883b
fuxinzhong <fuxinzhong@zhangyue.com>
2021-09-16 22:18:19 +0800

    【面试】

---
+++ leetcode/src/main/java /leetcode/editor/cn/ShuZuZhongDeNiXuDuiLcof  .java
@@ -0,0 +1,25 @@
+//在数组中的两个数字，如果前面一个数字大于后面的数字，则这两个数字组成一个逆序对。输入一个数组，求出这个数组中的逆序对的总数。 
+//
+// 
+//
+// 示例 1: 
+//
+// 输入: [7,5,6,4]
+//输出: 5 
+//
+// 
+//
+// 限制： 
+//
+// 0 <= 数组长度 <= 50000 
+// Related Topics 树状数组 线段树 数组 二分查找 分治 有序集合 归并排序 
+// 👍 522 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int reversePairs(int[] nums) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
