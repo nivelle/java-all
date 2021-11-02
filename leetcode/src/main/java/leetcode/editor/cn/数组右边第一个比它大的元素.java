ec1bc37fdfd594f50ea4cf30a4fd55d67fd7d9b7
fuxinzhong <fuxinzhong@zhangyue.com>
2021-11-02 23:15:17 +0800

    【leetcode】

--- leetcode/src/main/java/leetcode/editor/cn/数组右边第一个比它大的元素.java
+++ leetcode/src/main/java/leetcode/editor/cn/数组右边第一个比它大的元素.java
@@ -16,11 +16,15 @@ public class 数组右边第一个比它大的元素 {
         }
         int size = array.length;
         int[] result = new int[size];
+        //我们用栈来保存未找到右边第一个比它大的元素的索引（保存索引是因为后面需要靠索引来给新数组赋值），
+        //初始时，栈里放的是第一个元素的索引0值。
         Stack<Integer> stack = new Stack<>();
         stack.push(0);
         int index = 1;
         while(index < size) {
+            //找到第一个大于它的元素
             if(!stack.isEmpty() && array[index] > array[stack.peek()]) {
+                //该元素下标对应的值 是 array[index]
                 result[stack.pop()] = array[index];
             } else {
                 stack.push(index);
@@ -28,6 +32,7 @@ public class 数组右边第一个比它大的元素 {
             }
         }
         if(!stack.isEmpty()) {
+            //pop 移除
             result[stack.pop()] = -1;
         }
         return result;
